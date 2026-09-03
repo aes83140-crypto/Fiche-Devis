3ef769487031c97fe783df02fbbd6b67671c94b6
aes83140-crypto <aes83140@gmail.com>
2026-09-03 15:41:26 +0200

    mise a jour

--- config.js
+++ config.js
@@ -2,3 +2,8 @@
    Utilisé par la page d'accueil (mode administrateur) et par les fiches
    en construction (accès réservé). */
 window.RECORD_ADMIN_CODE = 'Chris04';
+
+/* Adresse de l'espace partagé (statistiques + listes des techniciens),
+   collée une fois pour toutes ici : plus besoin de jeton GitHub pour la diffuser,
+   toutes les pages et tablettes la connaissent dès qu'elles chargent ce fichier. */
+window.RECORD_STATS_URL = "https://script.google.com/macros/s/AKfycbylrX5f-BXY68CXQDa5tcKH16hnhUh9BDaekIv0kdFUftS1k1w3jx1iD4HXhnavLot1/exec";
